5067967d48b79d6b54a04ba2a357271525d21e8c
Julian Brost <julian@0x4a42.net>
2023-12-09 15:11:19 +0100

    2023: actually, part 1 and 2 of day 9 are pretty much the same

--- 2023/9.hs
+++ 2023/9.hs
@@ -2,21 +2,15 @@ parse c = map (map read . words) $ lines c
 
 deltas xs = zipWith (-) (tail xs) xs
 
-findNext xs
-    | all (==0) xs = 0
-    | otherwise    = (last xs) + (findNext $ deltas xs)
-
-solve1 = sum . map findNext
-
 findPrev xs
     | all (==0) xs = 0
     | otherwise    = (head xs) - (findPrev $ deltas xs)
 
-solve2 = sum . map findPrev
+solve = sum . map findPrev
 
 main :: IO ()
 main = do
     contents <- getContents
     let input = parse contents
-    putStrLn $ "Part 1: " ++ (show $ solve1 input)
-    putStrLn $ "Part 2: " ++ (show $ solve2 input)
+    putStrLn $ "Part 1: " ++ (show $ solve $ map reverse input)
+    putStrLn $ "Part 2: " ++ (show $ solve input)
