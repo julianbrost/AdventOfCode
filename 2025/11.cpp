8b7d7971761f44549d4f22add7dcf6b1efaa325f
Julian Brost <julian@0x4a42.net>
2025-12-11 09:40:51 +0100

    2025: simplify day 11 part 2

--- 2025/11.cpp
+++ 2025/11.cpp
@@ -44,23 +44,16 @@ int main() {
 		ts.emplace_back(move(c));
 	}
 
-	unordered_map<string,ull> p1 {{"you", 1}};
+	unordered_map<string,ull> p1{{"you", 1}}, p2{{"svr", 1}};
 	for (auto &c : ts) {
+		if (c == "dac" || c == "fft") {
+			p2 = {{c, p2[c]}};
+		}
 		for (const string &n : adj[c]) {
 			p1[n] += p1[c];
+			p2[n] += p2[c];
 		}
 	}
 	cout << "Part 1: " << p1["out"] << endl;
-
-	map<tuple<string,bool,bool>,ull> p2 {{{"svr", false, false}, 1}};
-	for (auto &c : ts) {
-		for (const string &n : adj[c]) {
-			for (bool dac : {false, true}) {
-				for (bool fft : {false, true}) {
-					p2[{n, dac || n == "dac", fft || n == "fft"}] += p2[{c, dac, fft}];
-				}
-			}
-		}
-	}
-	cout << "Part 1: " << p2[{"out", true, true}] << endl;
+	cout << "Part 2: " << p2["out"] << endl;
 }
